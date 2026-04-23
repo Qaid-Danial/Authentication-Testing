b0abf6530bb54b96a3930e5a54c2e497d1f89aed
Qaid-Danial <muhammadqaidd@gmail.com>
2026-04-24 00:12:26 +0800

    refactor: cleaned

---
+++ client.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+
+type Message struct {
+	TextMessage string `json:"textMessage"`
+}
+
+
+func ClientFunc() string {
+
+	var msg Message
+	var receive Message
+
+	Reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("> ")
+	read, _ := Reader.ReadString('\n')
+
+	msg = Message{
+		TextMessage: read,
+	}
+
+	marshalled, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Println("Unable to marshal: ", err)
+	}
+
+	resp, err := http.Post(
+		"http://localhost:8080/send",
+		"application/json",
+		bytes.NewBuffer(marshalled),
+	)
+	if err != nil {
+		fmt.Println("Unable to post: ", err)
+	}
+
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&receive); err != nil {
+		fmt.Println("Unable to decode message: ", err)
+	}
+
+	return ("Message from server: " + receive.TextMessage)
+
+}
--- main.go
+++ main.go
@@ -1,55 +1,11 @@
 package main
 
-import (
-	"bufio"
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-)
-
-type Message struct {
-	TextMessage string `json:"textMessage"`
-}
-
 func main() {
 
-	var msg Message
-	var receive Message
+	newWindow := CreateWindow()
 
-	Reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("> ")
-	read, _ := Reader.ReadString('\n')
-
-	msg = Message{
-		TextMessage: read,
+	if err := newWindow.Run(); err != nil {
+		panic(err)
 	}
-
-	marshalled, err := json.Marshal(msg)
-	if err != nil {
-		fmt.Println("Unable to marshal: ", err)
-		return
-	}
-
-	resp, err := http.Post(
-		"http://localhost:8080/send",
-		"application/json",
-		bytes.NewBuffer(marshalled),
-	)
-	if err != nil {
-		fmt.Println("Unable to post: ", err)
-		return
-	}
-
-	defer resp.Body.Close()
-
-	if err := json.NewDecoder(resp.Body).Decode(&receive); err != nil {
-		fmt.Println("Unable to decode message: ", err)
-		return
-	}
-
-	fmt.Println("Message from server: ", receive.TextMessage)
 
 }
